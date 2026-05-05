63d995508fd3847367e069e50d669dc6801e2490
Herman12-DEV <jesusestsauveur12@gmail.com>
2026-05-05 15:20:09 +0000

    modifier

--- bienvenue.cpp
+++ bienvenue.cpp
@@ -1,9 +1,23 @@
 // Affiche un message de bienvenue
+
 #include "fonction-bienvenue.h"
+#include <iostream>
+#include <string>
+#include <cstdlib>
 
-int main()
+int main(int argc, char* argv[])
 {
-  afficherBienvenue();
+    std::string message = "Bienvenue le monde !";
+    int nbAffichage = 1;
 
-  return 0;
+    if (argc > 1) {
+        message = argv[1];
+    }
+    if (argc > 2) {
+        nbAffichage = std::atoi(argv[2]);
+    }
+
+    afficherBienvenue(message, nbAffichage);
+
+    return 0;
 }
--- fonction-bienvenue.cpp
+++ fonction-bienvenue.cpp
@@ -1,7 +1,9 @@
 #include "fonction-bienvenue.h"
 #include <iostream>
 
-void afficherBienvenue(std::string message/*="Bienvenue le monde !"*/)
+void afficherBienvenue(std::string message, int nbAffichage)
 {
-    std::cout << message << std::endl;
+    for (int i = 0; i < nbAffichage; i++) {
+        std::cout << message << std::endl;
+    }
 }
